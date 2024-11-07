306160b6967bfb0828dca3dd82a7599c6e92bf04
Amouribadri <badriamouri2016@gmail.com>
2024-11-07 14:04:38 +0100

    add faq section

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,22 @@ $('#registration-form').submit(function(e){
 */
 
 smoothScroll.init();
+
+/*
+ * f&q action button 
+*/
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
